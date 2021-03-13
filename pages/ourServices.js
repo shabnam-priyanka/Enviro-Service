52f22e99e40009df880fa4d768447e7daddb364d
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-03-12 17:30:25 -0800

    @Mahbub-Hasan1

--- pages/ourServices.js
+++ pages/ourServices.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
+import { BookingContext } from '../components/Layout';
 
 const service = [
     {
@@ -62,23 +63,40 @@ const service = [
 
 
 const ourServices = () => {
+    const [bookingData, setBookingData] = useContext(BookingContext);
+    const serviceNameSetCheckout = (serviceName) => {
+        const AllBookingData = { ...bookingData };
+        AllBookingData.serviceName = serviceName;
+        setBookingData(AllBookingData);
+    };
+
     return (
         <div>
-        {
-            service.map(sr =>
-                <div className="card ourServiceText">
-                <img className="card-img ourservice" src={sr.img} alt="Card image"/>
-                <div className="card-img-overlay">
-                    <h5 className="card-title">{sr.name} </h5>
-                    <Link href="/bookings_checkout_calendar">
-                        <a>
-                            <button className="someReno_card-btn">Book for Consultation</button>
-                        </a>
-                    </Link>
-                </div>
-                </div>
-            )
-        }
+            {
+                service.map(sr =>
+                    <div key={sr.id} style={{backgroundImage: `url(${sr.img})`}} className="ourServiceCard d-flex align-items-center justify-content-center">
+                        <div>
+                            <h5>{sr.name}</h5>
+                            <Link  href="/bookings_checkout_calendar">
+                                <a onClick={() => serviceNameSetCheckout(sr.name)}>
+                                    <button>Book for Consultation</button>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                    // <div className="card ourServiceText">
+                    // <img className="card-img ourservice" src={sr.img} alt="Card image"/>
+                    // <div className="card-img-overlay">
+                    //     <h5 className="card-title">{sr.name} </h5>
+                    //     <Link href="/bookings_checkout_calendar">
+                    //         <a>
+                    //             <button className="someReno_card-btn">Book for Consultation</button>
+                    //         </a>
+                    //     </Link>
+                    // </div>
+                    // </div>
+                )
+            }
         </div>
     );
 };
